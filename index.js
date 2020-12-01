dff55f04bad8936a79b895c2c0f4587961fdb4b4
petrus766hi <petrus.sigiro@gmail.com>
2020-12-01 21:46:34 +0700

    cors

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const mongoDbConnection = require ('./config/db');
 const routes = require('./routes')
+var cors = require('cors')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoDbConnection()
+app.use(cors())
 app.use(routes)
 app.get('/', (req, res, next) => {
     res.send('Hello World')
