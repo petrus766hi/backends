5df25ed62f7cd011ce483c8f9889692a6fec2b40
petrus766hi <petrus.sigiro@gmail.com>
2020-12-05 17:08:35 +0700

    trid_commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,29 @@
 const jwt = require ('jsonwebtoken');
 
 module.exports = function (req, res, next) {
- const token = req.header('x-auth-token');
-  if (!token) {
-    return res.status(401).json ({msg: 'Token Invalid'});
-  }
+//  const token = req.header('x-auth-token');
+//   if (!token) {
+//     return res.status(401).json ({msg: 'Token Invalid'});
+//   }
 
-  try {
-    const decode = jwt.verify (token, 'jwtSecret');
-    req.user = decode.user;
-    next ();
-  } catch (err) {
-    res.status (401).json ({msg: 'Token anda tidak valid'});
-  }
+//   try {
+//     const decode = jwt.verify (token, 'jwtSecret');
+//     req.user = decode.user;
+//     next ();
+//   } catch (err) {
+//     res.status (401).json ({msg: 'Token anda tidak valid'});
+//   }
+    if(!req.headers.authorization) {
+      return res.status(401).send('Unauthorized request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if(token === 'null') {
+      return res.status(401).send('Unauthorized request')
+    }
+    let payload = jwt.verify(token, 'jwtSecret')
+    if(!payload) {
+      return res.status(401).send('Unauthorized request')
+    }
+    req.user = payload.user
+    next()
 };
